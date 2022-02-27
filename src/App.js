a701c22989f53290aa0bbc02dac21d02ddc1d1a1
Noah Milberger <n.milberger@gmail.com>
2022-02-27 16:50:56 -0500

    Put crypto api url into .env

--- src/App.js
+++ src/App.js
@@ -5,15 +5,7 @@ import AddCommas from "./utils/AddCommas";
 
 import "./App.css";
 
-const baseURL =
-  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
-
-// Puts commas in numbers
-// const numberWithCommas = (number) => {
-//   const parts = number.toString().split(".");
-//   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-//   return parts.join(".");
-// };
+const cryptoAPI = process.env.REACT_APP_CRYPTO_API;
 
 export default function App() {
   const [result, setResult] = useState([]);
@@ -25,7 +17,7 @@ export default function App() {
   const fetchData = async () => {
     const list = [];
     try {
-      await axios.get(baseURL).then((response) => {
+      await axios.get(cryptoAPI).then((response) => {
         for (var i in response.data) {
           list.push(response.data[i]);
         }
